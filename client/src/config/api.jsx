75254440929af25b6d9893136d70acf6623ee2e1
Sagar <rohit28sagar@gmail.com>
2025-11-02 18:01:23 +0530

    Adding Consolelogs in Api.jsx

--- client/src/config/api.jsx
+++ client/src/config/api.jsx
@@ -3,6 +3,9 @@ import Cookies from "js-cookie";
 
 // Prefer Vite env variable, fallback to the current render URL
 const BACKEND_URL = import.meta.env.VITE_BACKEND_URL
+console.log("Using Backend URL:", BACKEND_URL);
+console.log("Using Cookies:", Cookies.get("BhojanLoginKey"));
+
 
 const api = axios.create(
   { baseURL: `${BACKEND_URL}`, withCredentials: true },
